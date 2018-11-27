54d698655053a8c26ff34c2222e7862ff2e29314
SangHwa Kim <sanghwakim@SangHwaui-MacBook-Pro.local>
2018-11-28 08:09:09 +0900

    crawling code for analizing utilization

--- crawl/crawl_utilization.py
+++ crawl/crawl_utilization.py
@@ -13,11 +13,14 @@ def make_folder(folder_name) :
 path = './data'
 util_path = './data/utilization/'
 
-#chrawling
+#crawling
 for month in range(10, 12):
+    #10월 데이터 크롤링
     if month == 10 :
+        #10월 10일부터 10월 31일
         for day in range (10, 32):
             new_folder_name = util_path + str(month) + '_' + str(day)
+            #날짜별로 디렉토리 생성
             make_folder(new_folder_name)
             for hour in range(0, 24) :
                 time.sleep(1)
@@ -25,6 +28,8 @@ for month in range(10, 12):
                 if hour < 10:
                     page = ''
                     while page == '':
+                        # try-catch를 사용해 서버에서 접근을 못하게 할 경우 접근이 가능할 때까지 계속 접근을 시도함
+                        # 실제 크롤링이 수행되는 부분
                         try:
                             URL = 'https://datalab.naver.com/keyword/realtimeList.naver?datetime=2018-' + str(month) +'-' + str(day) + 'T0' + str(hour) + '%3A00%3A00'
                             print(URL)
@@ -44,7 +49,9 @@ for month in range(10, 12):
                     soup = BeautifulSoup(source,'html.parser')
                     top_list = soup.select("#content > div > div.keyword_carousel > div > div > div:nth-of-type(1) > div > div > ul > li > a > span")
                     new_file_name = new_folder_name + '/' + str(hour) +'.txt'
+                    # 새로운 파일 생성
                     new_file = open(new_file_name, 'w')
+                    # 새로 생긴 파일에 crawled data를 입력한다.
                     for top in top_list:
                         new_file.write(top.text)
                         new_file.write('\n')
@@ -74,8 +81,10 @@ for month in range(10, 12):
                     for top in top_list:
                         new_file.write(top.text)
                         new_file.write('\n')
-        
+
+    #11월 데이터..    
     else :
+        #11월 1일부터 11월  27일까지
         for day in range (1, 28):
             if(day < 10) :
                 new_folder_name = util_path + str(month) + '_0' + str(day)
@@ -89,6 +98,8 @@ for month in range(10, 12):
                 if hour < 10:
                     page = ''
                     while page == '':
+                        # try-catch를 사용해 서버에서 접근을 못하게 할 경우 접근이 가능할 때까지 계속 접근을 시도함
+                        # 실제 크롤링이 수행되는 부분
                         try:
                             URL = 'https://datalab.naver.com/keyword/realtimeList.naver?datetime=2018-' + str(month) +'-' + str(day) + 'T0' + str(hour) + '%3A00%3A00'
                             print(URL)
@@ -134,7 +145,9 @@ for month in range(10, 12):
                     soup = BeautifulSoup(source,'html.parser')
                     top_list = soup.select("#content > div > div.keyword_carousel > div > div > div:nth-of-type(1) > div > div > ul > li > a > span")
                     new_file_name = new_folder_name + '/' + str(hour) +'.txt'
+                    # 새로운 파일 생성
                     new_file = open(new_file_name, 'w')
+                    # 새로 생긴 파일에 crawled data를 입력한다.
                     for top in top_list:
                         new_file.write(top.text)
                         new_file.write('\n')
